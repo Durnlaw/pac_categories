ae9730401053114b19ef49d34419104d28b8c558
Durnlaw <durnlaw@hotmail.com>
2017-04-18 20:49:16 -0400

    Update OSAPI(Original).py

--- OSAPI(Original).py
+++ OSAPI(Original).py
@@ -1,7 +1,7 @@
 import urllib2
 import json
 
-OS_API = '32cabcec9c4b9305dc3b94acd5109c9e'
+OS_API = '_______'
 
 #url =''
 #above is provided by website. a JSON url code. This is for reference now. Repeatable URL is below.
