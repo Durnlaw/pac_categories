2b4ea76cef4a45fbb39ae15da23ce93104460c7c
Durnlaw <durnlaw@hotmail.com>
2020-01-21 01:04:56 -0500

    Need to sum the pac votes

--- vv_votes_2_PACs.py
+++ vv_votes_2_PACs.py
@@ -36,24 +36,71 @@ def vote_pvt(cycle):
     return pvt
 
 
+#> Run the function for each cycle of congress
 file = [113,114, 115, 116]
 
-#. Append them all together and print it out as a csv for later use.
 votes_list = []
 for x in file:
     print('Cycle: ', x)
     print('====================================================================================')
     print(' ')
     votes_list.append(vote_pvt(x))
-    # print(key_list.count())
 
+#. Concat them all together and ensure all columns align to ICPSR as the index.
 votes_as_columns = pd.concat(votes_list, axis = 1)
-# , ignore_index = True, sort=False
 print('Final shape:',votes_as_columns.shape)
-# print(votes_as_columns['icpsr'].unique().count())
-
 
 #! This thing is too large to really print to a csv usefully, so we are starting the next step of the proj in this same program
 #. Nice job. Print this sucker.
 # votes_as_columns.to_csv(
 #     path_or_buf = 'C:\\Programming\\repos\\Open-secrets\\data\\vv_votes_cols.csv')
+
+
+#> Now let's pull in leg_2010.csv. This will give us a list of all unique congresspeople, icpsr's, and opensecrets ids
+leg_2010_path = 'C:\\Programming\\repos\\Open-secrets\\data\\leg_2010.csv'
+leg_2010_key = pd.read_csv(leg_2010_path)
+leg_2010_key = leg_2010_key[['first', 'last', 'first_last', 'bioguide', 'icpsr', 'opensecrets']].drop_duplicates()
+print('Legislator dedup shape', leg_2010_key.shape)
+
+
+#> Now let's open "A FILE" that has the PAC information in it.
+#? Realistically this will have to be a pull of many files that we concat together
+
+os_pac_file_names = ['245', '250']
+
+os_pac_list = []
+for file in os_pac_file_names:
+    os_pac_path = 'C:\\Programming\\repos\\Open-secrets\\data\\os_pac\\{}.csv'.format(file)
+    os_pac_file = pd.read_csv(os_pac_path)
+
+    #. Select only needed rows
+    os_pac_data = os_pac_file[['org_name', 'indivs', 'pacs', 'total', 'cid']]
+    os_pac_list.append(os_pac_data)
+
+#. Concat them together
+pac_data = pd.concat(os_pac_list)
+print('PAC data shape:', pac_data.shape)
+
+#. Let's try to group the data up to reduce duplicates and preserve the $$. Index to false to preserve the grouped columns
+grouped_pac_data = pac_data.groupby(['org_name', 'cid'], as_index=False)[['indivs', 'pacs', 'total']].sum()
+print('PAC data shape:', grouped_pac_data.shape)
+print(grouped_pac_data.head(5))
+
+
+
+#> Alright, now we need to merge the other id's and names of each congressperson to the grouped_pac_data
+grouped_pac_cand_data = pd.merge(grouped_pac_data, leg_2010_key, left_on='cid', right_on='opensecrets')
+print('PAC data and congresspeople shape:', grouped_pac_cand_data.shape)
+print(grouped_pac_cand_data.head(5))
+
+
+
+#> Ok. Now let's get the vote data (in all it's glory) and attach it to the right of the grouped_pac_cand_data
+grouped_pac_cand_vote_data = pd.merge(grouped_pac_cand_data, votes_as_columns, on='icpsr')
+print('PAC data, congresspeople, votes shape:', grouped_pac_cand_vote_data.shape)
+# print(grouped_pac_cand_vote_data.head(5))
+
+sum_pac_vote_data = grouped_pac_cand_vote_data.iloc[pd.np.c_[0, 11:]]
+print(sum_pac_vote_data.shape)
+
+# grouped_pac_cand_vote_data.groupby(['org'])
