65163dd6ed7d9c994993b0ee680d39e715c30456
Ethan Winter <durnlaw@hotmail.com>
2020-02-21 22:24:44 -0500

    Notes update

--- pca_cluster.py
+++ pca_cluster.py
@@ -1,5 +1,4 @@
 
-#> Now that all of our data is together, we run PCA and clustering algorithms on the result of vv_votes_2_PACs.py
 
 import numpy as np
 import pandas as pd
@@ -16,12 +15,14 @@ import sklearn.metrics as sum_pac_vote_data
 from sklearn import datasets
 from sklearn.metrics import confusion_matrix, classification_report
 
+#> Now that all of our data is together, we run PCA and clustering algorithms on the result of vv_votes_2_PACs.py
+
 #. Let's oepn our main data file
-pac_vote_totals_path = 'C:\\Programming\\repos\\Open-secrets\\data\\sum_pac_vote_data.csv'
+pac_vote_totals_path = 'C:\\Coding\\repos\\pac_categories\\data\\sum_pac_vote_data.csv'
 
 
 #? We may need to come back to this, but for now, we need the label data as org_name
-# pac_vote_totals = pd.read_csv(pac_vote_totals_path, index_col='org_name')
+#// pac_vote_totals = pd.read_csv(pac_vote_totals_path, index_col='org_name')
 pac_vote_totals = pd.read_csv(pac_vote_totals_path)
 #! We are dropping these columns for now as they carry magnitude while the bill counts do not.
 pac_vote_totals = pac_vote_totals.drop(['indivs'], axis=1)
@@ -30,16 +31,17 @@ pac_vote_totals = pac_vote_totals.drop(['total'], axis=1)
 print(pac_vote_totals.shape)
 
 
-#. We will split the data along labels and features though.
+#. We will split the data along labels and features.
 pac_numbers = pac_vote_totals.drop('org_name', 1)
 pac_labels = pac_vote_totals['org_name']
-print(pac_numbers.shape)
-print(pac_labels.shape)
+print("PAC Numbers' Shape:", pac_numbers.shape)
+print("PAC Labels' Shape:", pac_labels.shape)
 
 #. Let's split into train and test for numbers and labels
 X_train, X_test, y_train, y_test = train_test_split(
     pac_numbers, pac_labels, test_size=0.2, random_state=0)
 
+#> Begin setting up PCA so that 
 #. PCA requires normalized data, so we will do standard scalar normalization
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
